d186a6d46dd0a277b38ada0e9c23356762310aa3
gokrusk <nigelljama@gmail.com>
2022-11-23 11:59:12 -0500

    subquerys, in, exists

--- TAREA6.sql
+++ TAREA6.sql
@@ -1,19 +1,19 @@
 --USE BD2
 
---1. Listar las materias (cod_materia y nombre) de las carreras de la FIE con m�s de 40 estudiantes
+--1. Listar las materias (cod_materia y nombre) de las carreras de la FIE con m s de 40 estudiantes
 SELECT m.cod_materia, m.nombre, count(cod_estudiante)
 FROM materia m
 JOIN matricula mt on mt.cod_materia = m.cod_materia
 JOIN carrera c on c.cod_carrera = m.cod_carrera
 WHERE cod_facultad = '001FIE'
 GROUP BY m.cod_materia, m.nombre
-HAVING count(cod_estudiante) >= 1
+HAVING count(cod_estudiante) > 40
 
---2. Listar las estudiantes (cod_estudiante, nombre) matriculados a BD1 que se encuentran con segunda matr�cula.
-SELECT e.cod_estudiante, nombre
+--2. Listar las estudiantes (cod_estudiante, nombre) matriculados a BD1 que se encuentran con segunda matricula.
+SELECT e.cod_estudiante, nombre, apellido
 FROM matricula mt
 JOIN estudiante e on mt.cod_estudiante = e.cod_estudiante
-GROUP BY e.cod_estudiante, nombre
+GROUP BY e.cod_estudiante, nombre, apellido
 HAVING count(mt.cod_materia) = 2
 
 --3. Conocer las materias (cod_materia y nombre) de la carrera de software cuyo promedio de nota (parcial1) es inferior al 75%
@@ -25,14 +25,34 @@ WHERE c.cod_carrera = '0001'
 GROUP BY m.cod_materia, m.nombre
 HAVING AVG(e.notaFinal) < 15
 
---4. Conocer las materias de la carrera de software con el mayor número de estudiantes con mayor igual a segunda  matriculas.
-SELECT m.nombre
+--4. Conocer las materias de la carrera de software con el mayor número de estudiantes con mayor igual a segundas matriculas.
+
+--estudiantes con 2 y mas matriculas
+SELECT cod_estudiante, cod_materia
+FROM matricula
+GROUP by cod_estudiante, cod_materia
+HAVING count(cod_estudiante) >= 2
+
+--cantidad de estudiantes por materia de software
+SELECT m.nombre, count(cod_estudiante)
 FROM materia m
 JOIN carrera c on m.cod_carrera = c.cod_carrera
 JOIN matricula mt on mt.cod_materia = m.cod_materia
 WHERE c.cod_carrera = '0001'
 GROUP BY m.nombre
-HAVING count(mt.cod_materia) = 2 
+
+SELECT cod_estudiante, cod_materia, count(cod_estudiante)
+FROM matricula mt
+WHERE EXISTS (
+	SELECT m.nombre, count(cod_estudiante)
+	FROM materia m
+	JOIN carrera c on m.cod_carrera = c.cod_carrera
+	JOIN matricula mt on mt.cod_materia = m.cod_materia
+	WHERE c.cod_carrera = '0001'
+	GROUP BY m.nombre
+)
+GROUP by cod_estudiante, cod_materia
+HAVING count(cod_estudiante) >= 2
 
 --5. Conocer las materias cuya nota promedio (nota1) sea inferior al promedio general de la carrera de software
 SELECT nombre, nota1
@@ -40,7 +60,7 @@ FROM evaluacion e
 JOIN materia m on m.cod_materia = e.cod_materia
 WHERE nota1 < 
 	(
-		SELECT AVG(nota1) 
+		SELECT AVG(notaFinal) 
 		FROM evaluacion e
 		JOIN materia m on e.cod_materia = m.cod_materia
 		JOIN carrera c on c.cod_carrera = m.cod_carrera
@@ -58,3 +78,8 @@ ORDER BY 1
 --7. Conocer la tasa de repitencia por materia (Número de estudiantes matriculados en la materia 
 --/ número de estudiantes que han reprobado dicha materia) de la carrera de software en un periodo académico específico.
 
+SELECT mt.cod_materia, COUNT(cod_estudiante)--/COUNT(mt.cod_materia)--/(SELECT COUNT(cod_materia) FROM matricula GROUP BY cod_materia HAVING COUNT(cod_materia) >= 2)
+FROM matricula mt
+JOIN materia m on m.cod_materia = mt.cod_materia
+WHERE m.cod_carrera = '0001' and mt.cod_periodo = '2122'
+GROUP BY mt.cod_materia
---
+++ sql15.sql
@@ -0,0 +1,41 @@
+--USE BD2_NJ
+
+--LISTA DE ESTUDIANTES QUE HAN APROBADO UNA MATERIA ESPECIFICA EN UN PERIODO ESPECIFICO
+SELECT ev.cod_estudiante, e.nombre, e.apellido--, ev.cod_periodo
+FROM evaluacion ev
+JOIN materia m on ev.cod_materia = m.cod_materia
+JOIN estudiante e on ev.cod_estudiante = e.cod_estudiante
+WHERE ev.cod_materia = '0001' and estado = '002' and cod_periodo = '1920'
+
+--any al menos con uno 
+SELECT e.cod_estudiante, e.nombre, e.apellido
+FROM estudiante e
+WHERE e.cod_estudiante = ALL(
+		SELECT cod_estudiante
+		FROM evaluacion
+		WHERE cod_materia = '0001' and estado = '002' and cod_periodo = '1920'
+		)
+
+--NOTA MAXIMA Y NOTA MINIMA POR CADA MATERIA
+SELECT MAX(notaFinal)
+FROM evaluacion ev
+
+SELECT MIN(notaFinal)
+FROM evaluacion ev
+
+--SUBQUERY CORRELACIONADA
+SELECT DISTINCT m.cod_materia, nombre, 
+	(SELECT MAX(notaFinal) FROM evaluacion ev WHERE ev.cod_materia = m.cod_materia), 
+	(SELECT MIN(notaFinal) FROM evaluacion ev WHERE ev.cod_materia = m.cod_materia)
+FROM materia m
+JOIN evaluacion ev on m.cod_materia = ev.cod_materia
+
+--CONOCER TODOS LOS ESTUDIANTES QUE NO SE HAN MATRICULADO
+SELECT E.cod_estudiante, nombre, apellido
+FROM estudiante e
+LEFT JOIN matricula m on e.cod_estudiante = m.cod_estudiante
+WHERE m.cod_estudiante IS NULL
+
+SELECT cod_estudiante, nombre, apellido
+FROM estudiante
+WHERE cod_estudiante NOT IN (SELECT DISTINCT cod_estudiante	FROM matricula)
